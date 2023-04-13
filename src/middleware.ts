02c8d14cde42f8c8679e33c05d759e5ac6571708
Red Chien <redchien@viridiasolutions.com>
2023-04-13 15:42:14 +0800

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { Middleware } from "redux";
+import { RootState } from "./store";
+
+/** 觀察這三個 middleware 執行後，console.log 的順序 */
+
+export const logger: Middleware<{}, RootState> = (store) => (next) => (action) => {
+    //dispatch 時要執行的動作
+    console.log("dispatching", action);
+    let result = next(action);
+    //dispatch 後要執行的動作
+    console.log("next state", store.getState());
+    return result;
+};
+
+export const customMiddleware: Middleware<{}, RootState> = (store) => (next) => (action) => {
+    console.log("custom dispatching")
+    let result = next(action);
+    console.log("after custom dispatching")
+    return result;
+}
+
+//error handling middleware
+export const crashReporter: Middleware<{}, RootState> = (store) => (next) => (action) => {
+    try {
+        console.log("crashReporter dispatching")
+        let result = next(action)
+        console.log("crashReporter next state", store.getState())
+        return result;
+    } catch (err) {
+        console.error("Caught an exception!", err);
+        throw err;
+    }
+};
--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from './slices/todo'
+import { logger, customMiddleware, crashReporter } from './middleware'
+import { combineReducers } from 'redux'
 
-const store = configureStore({
-    reducer: {
-        todoReducer
-    }
+const reducers = combineReducers({
+    todoReducer
 })
 
-export type RootState = ReturnType<typeof store.getState>
+const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger, customMiddleware, crashReporter)
+})
+
+// 尚未 combine reducers 和使用 middleware 時的寫法
+// export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof reducers>
 export type AppDispatch = typeof store.dispatch
 
 export default store
